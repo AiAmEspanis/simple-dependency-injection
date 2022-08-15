29b9c7845f444b00812ed957a2303aa6b6656ff8
AiAmEspanis <rafaperez.software@gmail.com>
2022-08-15 18:09:45 +0200

    remove new code and set dev version

--- simple_dependency_injection/dependency_container.py
+++ simple_dependency_injection/dependency_container.py
@@ -124,12 +124,3 @@ class DependencyContainerFake:
 
     def reset(self):
         self._dependency_container.restore(self._old_state)
-
-    def test(self):
-        return 1
-
-    def test_2(self):
-        return 1
-
-    def test_3(self):
-        return 1
